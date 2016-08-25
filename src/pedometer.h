150c2ef0dd2726d026b272774f8538631ebce76f
bzhtapp <bzhtapp@gmail.com>
2016-08-25 13:58:27 -0400

    Remove pedometer timestamp diff

--- src/pedometer.h
+++ src/pedometer.h
@@ -1,8 +1,7 @@
 const uint32_t s_step_key = 1;
 const uint32_t s_samples_per_update = 5;
-const uint16_t s_step_min_diff = 400;
-const uint16_t s_step_timeStamp_diff_min = 150;
-const uint16_t s_step_threshold = 550;
+const uint16_t s_step_min_diff = 300;
+const uint16_t s_step_threshold = 600;
 const AccelSamplingRate s_acelSamplingRate = ACCEL_SAMPLING_10HZ;
 
 static void update_step(int steps);
--- worker_src/worker.c
+++ worker_src/worker.c
@@ -2,9 +2,6 @@
 #include <pedometer.h>
 
 static uint16_t s_step_gForce;
-static time_t s_timeStamp;
-static int s_timeStamp_diff;
-
 
 static void update_step(int steps){
   int step_count = persist_read_int(s_step_key);
@@ -19,29 +16,18 @@ static void accel_data_handler(AccelData *data, uint32_t num_samples){
     // Read first sample gForce
   int16_t gForce = data[0].y;
   bool did_vibrate = data[0].did_vibrate;
-  time_t timeStamp = data[0].timestamp;
   
   //default number of credited steps per event
   int steps = 1;
   
-  //initialize static timeStamp
-  if (!s_timeStamp) {
-    s_timeStamp = timeStamp;
-  }
-  
-  s_timeStamp_diff = timeStamp - s_timeStamp;  
-  
   //Count as a step if difference between last stored value and current value exceed minimum difference
   //and current gForce above threshold
   //and watch did not vibrate
-  //and time different between previous sample more than 200 millisecond
   if ( abs(gForce - s_step_gForce) > s_step_min_diff
       && abs(gForce) > s_step_threshold 
-     && !did_vibrate
-     && s_timeStamp_diff > s_step_timeStamp_diff_min) 
+     && !did_vibrate) 
   {
     update_step(steps); 
-    s_timeStamp = timeStamp;
   }
   
   //store new current value
