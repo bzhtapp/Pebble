617b9947cc4994ca6655cfa923a172e83d31932e
bzhtapp <bzhtapp@gmail.com>
2016-08-27 09:47:01 -0400

    Lowered step sample rate to 4 per batch

--- src/pedometer.h
+++ src/pedometer.h
@@ -1,5 +1,5 @@
 const uint32_t s_step_key = 1;
-const uint32_t s_samples_per_update = 5;
+const uint32_t s_samples_per_update = 4;
 const uint16_t s_step_min_diff = 300;
 const uint16_t s_step_threshold = 600;
 const AccelSamplingRate s_acelSamplingRate = ACCEL_SAMPLING_10HZ;
